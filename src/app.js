589d9578a6e553fae7ac8bf8b0345181dd9ec242
ruhitbaidya <ruhitbaidya01@gmail.com>
2025-04-04 01:09:14 +0600

    projet some issue done

--- src/app.js
+++ src/app.js
@@ -4,12 +4,7 @@ import { rootRouter } from "./router/routes.js";
 import nodemailer from "nodemailer";
 const app = express();
 const corsOptions = {
-  origin: [
-    "http://localhost:5174",
-    "http://localhost:5173",
-    "https://my-dashboard-sigma-ochre.vercel.app",
-    "https://ruhitbaidya.vercel.app",
-  ],
+  origin: "*",
   credentials: true,
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
